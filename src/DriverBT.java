195a7b52207b1871bd57c2ad104d8f2e04e7426b
Kevin Hoang <hevinkoang@gmail.com>
2021-10-23 00:38:11 -0700

    implemented createTree2

--- src/DriverBT.java
+++ src/DriverBT.java
@@ -5,13 +5,13 @@ public class DriverBT
 		System.out.println("1st Testing Example:");
 		BinaryTree<String> aTree = new BinaryTree<>();
 		createTree1(aTree);
-		
+
 		System.out.println("(binaryTree) post-order: ");
 		aTree.postorderTraverse();
-		
+
 		System.out.println("(binaryNode) post-order: ");
 		aTree.postorderTraverse_callBinaryNodeMethod();
-		
+
 		System.out.println();
 		
 		System.out.println("(BinaryTree) Height of tree is " + aTree.getHeight());
@@ -28,13 +28,13 @@ public class DriverBT
 		System.out.println("2nd Testing Example:");
 		BinaryTree<String> aTree2 = new BinaryTree<>();
 		createTree2(aTree2);
-		
+
 		System.out.println("(binaryTree) post-order: ");
 		aTree2.postorderTraverse();
-		
+
 		System.out.println("(binaryNode) post-order: ");
 		aTree2.postorderTraverse_callBinaryNodeMethod();
-		
+
 		System.out.println();
 		
 		System.out.println("(BinaryTree) Height of tree is " + aTree2.getHeight());
@@ -76,7 +76,29 @@ public class DriverBT
 	/**-------------------------------------------------------------------- */
 	/**Task 4: Create a tree case as shown in assignment 3*/
 	public static void createTree2(BinaryTree<String> tree)
-	{ 
+	{
+		// Leaves
+		BinaryTree<String> fTree = new BinaryTree<>("F");
+		BinaryTree<String> bTree = new BinaryTree<>("B");
+		BinaryTree<String> gTree = new BinaryTree<>("G");
+		BinaryTree<String> hTree = new BinaryTree<>("H");
+
+		//Subtrees
+		BinaryTree<String> dTree = new BinaryTree<>("D", fTree, null);
+		BinaryTree<String> eTree = new BinaryTree<>("E", gTree, hTree);
+		BinaryTree<String> cTree = new BinaryTree<>("C", dTree, eTree);
+
+		tree.setTree("A", bTree, cTree);
+
+		System.out.println("\nGiven Tree:\n");
+		System.out.println("     A      ");
+		System.out.println("   /   \\  ");
+		System.out.println("  B     C  ");
+		System.out.println("       /  \\ ");
+		System.out.println("      D    E");
+		System.out.println("     /    / \\");
+		System.out.println("    F    G   H");
+		System.out.println();
 
 	} // end createTree2
 
