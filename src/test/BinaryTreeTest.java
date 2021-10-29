ce091804d90530f2afe0708441285fe4fa25a7a7
kevinhoang0 <89752786+kevinhoang0@users.noreply.github.com>
2021-10-28 18:46:30 -0700

    Create BinaryTreeTest.java

---
+++ src/test/BinaryTreeTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryTreeTest {
+
+    @Test
+    void postorderTraverse() {
+    }
+
+    @Test
+    void postorderTraverse_callBinaryNodeMethod() {
+    }
+
+    /**
+     * Normal binary tree test case
+     */
+    @Test
+    void getHeight_callBinaryNodeMethod() {
+        BinaryTree<String> dTree = new BinaryTree<>("D");
+        BinaryTree<String> eTree = new BinaryTree<>("E");
+        BinaryTree<String> bTree = new BinaryTree<>("B",dTree,eTree);
+        BinaryTree<String> cTree = new BinaryTree<>("C");
+        BinaryTree<String> aTree = new BinaryTree<>("A",bTree,cTree);
+
+        // Tree: A
+        //      / \
+        //     B   C
+        //    /  \
+        //   D    E
+        int test = aTree.getHeight_callBinaryNodeMethod();
+        assertEquals(3,test);
+    }    /**
+     * Null binary tree test case
+     */
+    @Test
+    void getHeight_callBinaryNodeMethod2() {
+        BinaryTree<String> aTree = new BinaryTree<>();
+
+
+        int test = aTree.getHeight_callBinaryNodeMethod();
+        assertEquals(0,test);
+    }
+
+
+    @Test
+    void getNumberOfNodes() {
+    }
+}
